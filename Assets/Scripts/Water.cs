1b7e119105059a7511491124a5ea44d521beecbf
MRVNY <MRVNY@users.noreply.github.com>
2020-12-27 17:54:54 +0100

    17:54

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : IElement
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void activate(IElement interactable)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void react(IElement activator)
+    {
+        throw new System.NotImplementedException();
+    }
+}
